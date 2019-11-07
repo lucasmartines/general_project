b412551b4c25aa1a447970c4dc9a5656ac787c19
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-11-07 04:55:59 -0200

    resolvendo bug https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
- 
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,7 +29,8 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
-      
+        URL::forceScheme('https');
+
 
     }
 }
